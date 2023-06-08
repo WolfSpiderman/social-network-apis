45bf9a79794ac19d9eeeb13d4f34842c01fc553e
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-08 16:31:18 -0500

    added skeleton controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,39 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getThoughts(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getOneThought(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createThought(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateThought(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteThought(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async addReaction(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    }, 
+    async removeReaction(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    async getUsers(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async getOneUser(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async createUser(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteUser(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async addFriend(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    }, 
+    async removeFriend(req, res) {
+        try {} catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
