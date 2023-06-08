766fe693be138b1f073a8c2463e03534d9be4e6d
WolfSpiderman <wolfspiderman@yahoo.com>
2023-06-08 17:28:19 -0500

    getUsers, getOneUser, and createUser written and tested

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,17 +2,32 @@ const { User, Thought } = require('../models');
 
 module.exports = {
     async getUsers(req, res) {
-        try {} catch (err) {
+        try {
+            const users = await User.find();
+            res.json(users);
+        } catch (err) {
             res.status(500).json(err);
         }
     },
     async getOneUser(req, res) {
-        try {} catch (err) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+            .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that ID.' });
+            }
+
+            res.json(user);
+        } catch (err) {
             res.status(500).json(err);
         }
     },
     async createUser(req, res) {
-        try {} catch (err) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
             res.status(500).json(err);
         }
     },
